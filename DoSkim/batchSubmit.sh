09c74ea582ab3ea09cdbd653c3ae560f09ddcf25
abdollah110 <smohamadi@gmail.com>
2015-12-21 15:58:59 -0600

    rm DoSkim

--- DoSkim/batchSubmit.sh
+++
@@ -1,7 +0,0 @@
-cd /afs/cern.ch/work/a/abdollah/scratch1/2015NtupleProduction/ggNtuple/CMSSW_7_4_1/src/ggAnalysis/ggNtuplizer/DoSkim
-eval `scram runtime -sh`
-xrdcp  root://eoscms.cern.ch//eos/cms/store/group/phys_smp/ggNtuples/13TeV/mc/job_spring15_DYJetsToLL_m50_25ns.root /tmp/job_spring15_DYJetsToLL_m50_25ns.root
-
-./Skimmer /tmp/  job_spring15_DYJetsToLL_m50_25ns.root   5
-
-
