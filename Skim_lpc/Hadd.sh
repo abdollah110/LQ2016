9d1fd013d3f8ac85d88e68005f89d867da3aa7d0
abdollah110 <smohamadi@gmail.com>
2016-01-26 15:32:44 -0600

    skim lpc

---
+++ Skim_lpc/Hadd.sh
@@ -0,0 +1,10 @@
+hadd WWTo2L2Nu.root  *_spring15_WWTo2L2Nu_powheg_25ns_miniAOD.root
+hadd WZTo3LNu.root  *_spring15_WZTo3LNu_powheg_pythia8_25ns_miniAOD.root
+hadd ZZTo4L.root  *_spring15_ZZTo4L_amcatnlo_pythia8_25ns_miniAOD.root
+hadd DYJetsToLL.root  *_spring15_ggNtuple_DYJetsToLL_M-50_amcatnlo_pythia8_25ns_miniAOD.root
+hadd TTJets.root  *_spring15_ggNtuple_TTJets_amcatnlo_pythia8_25ns_miniAOD.root
+hadd WJetsToLNu.root  *_spring15_ggNtuple_WJetsToLNu_amcatnlo_pythia8_25ns_miniAOD.root
+hadd SingleEle.root  *_data_ggNtuple_SingleElectron*.root
+hadd SingleMu.root  *_data_ggNtuple_SingleMuon*.root
+
+
